ecd49b9200de0ed9e9f81526c60316899a82c8c3
HeRedBo <hhbjkd@163.com>
2017-03-02 21:02:51 +0800

    添加php service 层知识点 增加 service工厂类

---
+++ Service/ServiceFactory.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * service 工厂类 用来获取指定的service 对象
+ * yii 中定义sevice基类 用于数据的其他service 的载入
+ */
+class ServiceFactory
+{
+    public static $_services = [];
+
+    public static function getService($class)
+    {
+        $className = ucfirst($class).'Service';
+        if(!isset(self::$_services[$className]))
+        {
+            return self::$_services[$className] = new $className();
+        }
+        else
+        {
+            return $_services[$className];
+        }
+    }
+
+}
---
+++ Service/factory.php
@@ -0,0 +1,30 @@
+<?php
+
+// 工厂模式
+class factory
+{
+    public static $_services = [];
+
+    /**
+     * 生产对象
+     * @param  string $class 类名
+     * @return object|bool
+     */
+    public static function getInstance($class)
+    {
+        // 加载生产类
+        $className = ucfirst($class).'.Service';
+        if(!isset(self::$_services))
+        {
+            if(file_exists($className.'.class.php'))
+            {
+                require_once $className.'.class.php';
+                return new $className();
+            }
+        }
+        else
+        {
+            return $_services[$className];
+        }
+    }
+}
