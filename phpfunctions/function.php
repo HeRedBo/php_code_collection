008934d54247d57679604431a1b25d02c8dadc72
HeRedBo <hhbjkd@163.com>
2017-07-26 09:23:18 +0800

    增加银行卡校验与身份证

--- phpfunctions/function.php
+++ phpfunctions/function.php
@@ -238,4 +238,117 @@ function generateRandomStr($length = 8)
     return  substr($str, 0, $length);
 }
 
+/**
+ * 校验银行卡号是否输入有误 使用了Luhn算法校验
+ * @param  int $no 银行卡号
+ * @return bool 返回数据校验结果
+ */
+function luhm($no)
+{
+	if(empty($no) || !is_numeric($no) || strlen($no) < 16)
+	{
+		return false;
+	}
+	$arr_no = str_split($no);
+	$last_n = end($arr_no);
+	krsort($arr_no);
+	$i = 1;
+	$total = 0;
+	foreach ($arr_no as $n)
+	{
+	    if($i%2==0) {
+	        $ix = $n*2;
+	        if($ix >= 10) {
+	            $nx = 1 + ($ix % 10);
+	            $total += $nx;
+	        } else {
+	            $total += $ix;
+	        }
+	    } else {
+	        $total += $n;
+	    }
+	    $i++;
+	}
+	$total -= $last_n;
+	$total *= 9;
+	if($last_n == ($total%10)){
+    	return true;
+	} 
+	return false;
+}
 
+
+/**
+ * 身份证校验函数 start
+ */
+
+function validation_filter_id_card($id_card)
+{
+    if(strlen($id_card)==18)
+    {
+        return idcard_checksum18($id_card);
+    }
+    elseif((strlen($id_card)==15))
+    {
+        $id_card=idcard_15to18($id_card);
+        return idcard_checksum18($id_card);
+    }
+    else
+    {
+        return false;
+    }
+}
+
+// 计算身份证校验码，根据国家标准GB 11643-1999
+function idcard_verify_number($idcard_base)
+{
+    if(strlen($idcard_base)!=17){
+        return false;
+    }
+    //加权因子
+    $factor=array(7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2);
+    //校验码对应值
+    $verify_number_list=array('1','0','X','9','8','7','6','5','4','3','2');
+    $checksum=0;
+    for($i=0;$i<strlen($idcard_base);$i++) {
+        $checksum += substr($idcard_base,$i,1) * $factor[$i];
+    }
+    $mod=$checksum % 11;
+    $verify_number=$verify_number_list[$mod];
+    return $verify_number;
+}
+// 将15位身份证升级到18位
+function idcard_15to18($idcard)
+{
+    if(strlen($idcard)!=15){
+        return false;
+    }else{
+        // 如果身份证顺序码是996 997 998 999，这些是为百岁以上老人的特殊编码
+        if(array_search(substr($idcard,12,3),array('996','997','998','999')) !== false){
+            $idcard=substr($idcard,0,6).'18'.substr($idcard,6,9);
+        }else{
+            $idcard=substr($idcard,0,6).'19'.substr($idcard,6,9);
+        }
+    }
+    $idcard=$idcard.idcard_verify_number($idcard);
+    return $idcard;
+}
+// 18位身份证校验码有效性检查
+function idcard_checksum18($idcard)
+{
+    if(strlen($idcard)!=18) {
+        return false;
+    }
+    $idcard_base=substr($idcard,0,17);
+    if(idcard_verify_number($idcard_base)!= strtoupper(substr($idcard,17,1))) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+//调用方法如：
+$res = validation_filter_id_card('44142319940113561X');
+/**
+ * 身份证校验函数 end 
+ */
